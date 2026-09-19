385f7e8e8e4055ad5173ab83a10933f0ad79998e
a.kakhuashvili <a.kakhuashvili@a1qa.com>
2026-09-20 01:44:37 +0400

    Commit #1: BankAccount class with deposit() and getBalance() implemented, and the broken withdraw() in place

---
+++ BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class BankAccount
+{
+	public BankAccount()
+	{
+		private double balance;
+		private string owner;
+
+        public void Withdraw(double amount)
+        {
+            // BUG: Does not check for negative amounts!
+            if (amount > balance)
+            {
+                throw new ArgumentException("Insufficient funds!");
+            }
+            balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            balance -= amount;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
